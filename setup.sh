ca9b3a25fc882a636f47e428b07548656a4b442b
Brett Dutro <brett.dutro@gmail.com>
2021-01-05 20:37:55 -0600

    Ensure we use Python 3 venv and pip

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
+PYTHON3=/usr/bin/env python3
+$PYTHON3 -m venv .env && source .env/bin/activate && $PYTHON3 -m pip install -r requirements.txt
 
 sudo cp $SCRIPT_PATH/chromium-proxy.conf.example /etc/chromium-proxy.conf
 
