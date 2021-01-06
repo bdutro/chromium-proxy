c11420f0e250df0ff45e39459fb9a325d4e6a028
Brett Dutro <brett.dutro@gmail.com>
2021-01-05 20:55:46 -0600

    Automatically create user Add systemd unit

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PYTHON3="/usr/bin/env python3"
+PROXY_USER=cproxy
 $PYTHON3 -m venv .env && source .env/bin/activate && $PYTHON3 -m pip install -r requirements.txt
 
 sudo cp $SCRIPT_PATH/chromium-proxy.conf.example /etc/chromium-proxy.conf
@@ -11,3 +12,11 @@ export PYPPETEER_HOME
 
 USER_DIR=$($SCRIPT_PATH/user_dir.py)
 sudo mkdir -p $USER_DIR
+
+if ! id $PROXY_USER &>/dev/null; then
+    sudo useradd -M -U -s /usr/sbin/nologin $PROXY_USER
+fi
+
+sudo chown -R $PROXY_USER:$PROXY_USER $USER_DIR/..
+sudo cp $SCRIPT_PATH/chromium-proxy.service /etc/systemd/system/chromium-proxy.service
+sudo sed -i "s/%INSTALL_PATH%/${SCRIPT_PATH}/g" /etc/systemd/system/chromium-proxy.service
